8a0ffe0cce7e32873148d80c385f15e26712e008
ZYGYU <163495115+ZYGYU@users.noreply.github.com>
2025-01-25 09:05:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ for TIKTOK_LINK in TIKTOK_LINKS:
                 # Fallback: Mengunggah ke Rclone jika Telegram gagal
                 subprocess.run( rclone_command = [
                     'rclone', 'move', os.path.join(VIDEO_DIR, video_file),
-                    'b2:Kodi-Media/Movies/', '-v', '--progress', '--config rclone.config'
+                    'b2:Kodi-Media/Movies/', '-v', '--progress', '--config rclone.conf'
                 ])
                 rclone_result = subprocess.run(rclone_command, capture_output=True, text=True)
                 if rclone_result.returncode == 0:
